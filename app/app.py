4cc8319ea1bcb35e935a69c6a8c3524f90b22aca
Jay K <jkassis87@gmail.com>
2023-09-24 22:57:55 +1000

    added CRUD opperations

--- app/app.py
+++ app/app.py
@@ -21,26 +21,7 @@ def dict_factory(cursor, row):
 def index():
     return render_template('index.html')
 
-@app.route('/tracks')
-def get_tracks_page():
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM tracks")
-    tracks = cursor.fetchall()
-    conn.close()
-    return render_template('tracks.html', tracks=tracks)
-
-# API routes for tracks
-@app.route('/api/tracks', methods=['GET'])
-def get_tracks():
-    conn = connect_db()
-    conn.row_factory = dict_factory
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM tracks")
-    tracks = cursor.fetchall()
-    conn.close()
-    return jsonify(tracks)
-
+# Read a specific track (GET endpoint)
 @app.route('/api/tracks/<int:track_id>', methods=['GET'])
 def get_track(track_id):
     conn = connect_db()
@@ -52,6 +33,76 @@ def get_track(track_id):
     if track is None:
         return jsonify({"error": "Track not found"}), 404
     return jsonify(track)
+
+# Create a new track (POST endpoint)
+@app.route('/api/tracks', methods=['POST'])
+def create_track():
+    data = request.json
+    if not data or 'Name' not in data or 'AlbumId' not in data or 'MediaTypeId' not in data:
+        return jsonify({"error": "Invalid data"}), 400
+
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO tracks (Name, AlbumId, MediaTypeId) VALUES (?, ?, ?)",
+                   (data['Name'], data['AlbumId'], data['MediaTypeId']))
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Track created successfully"}), 201
+
+# Update a track (PUT Endpoint)
+@app.route('/api/tracks/<int:track_id>', methods=['PUT'])
+def update_track(track_id):
+    data = request.json
+    if not data or ('Name' not in data and 'AlbumId' not in data and 'MediaTypeId' not in data):
+        return jsonify({"error": "Invalid data"}), 400
+
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM tracks WHERE TrackId = ?", (track_id,))
+    track = cursor.fetchone()
+    if track is None:
+        conn.close()
+        return jsonify({"error": "Track not found"}), 404
+
+    update_query = "UPDATE tracks SET "
+    update_data = []
+    if 'Name' in data:
+        update_query += "Name = ?, "
+        update_data.append(data['Name'])
+    if 'AlbumId' in data:
+        update_query += "AlbumId = ?, "
+        update_data.append(data['AlbumId'])
+    if 'MediaTypeId' in data:
+        update_query += "MediaTypeId = ?, "
+        update_data.append(data['MediaTypeId'])
+
+    update_query = update_query.rstrip(', ')
+    update_query += " WHERE TrackId = ?"
+    update_data.append(track_id)
+
+    cursor.execute(update_query, tuple(update_data))
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Track updated successfully"})
+
+# Delete a track (DELETE endpoint)
+@app.route('/api/tracks/<int:track_id>', methods=['DELETE'])
+def delete_track(track_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM tracks WHERE TrackId = ?", (track_id,))
+    track = cursor.fetchone()
+    if track is None:
+        conn.close()
+        return jsonify({"error": "Track not found"}), 404
+
+    cursor.execute("DELETE FROM tracks WHERE TrackId = ?", (track_id,))
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Track deleted successfully"})
 
 if __name__ == '__main__':
     app.run(debug=True)
